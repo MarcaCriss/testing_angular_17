80f0de66fd7c96142e9f1a3aee0e552831d6f997
Criss_Mark_ <cristianmarca70@gmail.com>
2024-02-10 22:59:52 -0400

    feat: testing dependency injection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +10,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'test_angular_17';
+  private router = inject(Router);
+
+  redirectHome() {
+    this.router.navigate(['home']);
+  }
 }
